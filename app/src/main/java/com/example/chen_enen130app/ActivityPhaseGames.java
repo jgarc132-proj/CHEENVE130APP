2d9271e601bac8d49dbb6cefaf9c5bb28ad65f69
Jose Garcia <jgarc132@ucr.edu>
2020-08-04 13:40:57 -0700

    Updated layout file for activity_phase_games.xml and allowed activity to open from 'phase games' activity using 'Raoult's Law' calculator button.

--- app/src/main/java/com/example/chen_enen130app/ActivityPhaseGames.java
+++ app/src/main/java/com/example/chen_enen130app/ActivityPhaseGames.java
@@ -2,7 +2,10 @@ package com.example.chen_enen130app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityPhaseGames extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class ActivityPhaseGames extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phase_games);
+
+        Button RLButton = (Button) findViewById(R.id.RLButton);
+        RLButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RaoultsLawCalculator.class);
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/chen_enen130app/RaoultsLawCalculator.java
@@ -0,0 +1,14 @@
+package com.example.chen_enen130app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RaoultsLawCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raoults_law_calculator);
+    }
+}
